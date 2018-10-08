76e620dfbf39ba9dfbb8f1ccb2ebb7e14f7b70e3
Marc-Aurèle Brothier <m@brothier.org>
2018-10-08 10:19:45 +0200

    zsh: fix script to copy SSH key

--- roles/user-packages/zsh/files/system/keys.zsh
+++ roles/user-packages/zsh/files/system/keys.zsh
@@ -1,2 +1,6 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+else
+	alias pubkey="cat ~/.ssh/id_rsa.pub | xclip -sel clip"
+fi
