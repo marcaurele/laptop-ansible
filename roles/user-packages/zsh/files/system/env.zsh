ca84e27c53229e71b538db1a84b8e51730a9bebb
Marc-Aurèle Brothier <m@brothier.org>
2019-01-21 06:43:54 +0100

    zsh: fix for ssh-agent under linux

--- roles/user-packages/zsh/files/system/env.zsh
+++ roles/user-packages/zsh/files/system/env.zsh
@@ -5,9 +5,9 @@ export EDITOR='vim'
 # Correctly set the locale
 # I'd rather have a en locale than a french one
 # and I don't like the US format, so CA is a good option
-export LANG='en_US.UTF-8'
-export LC_CTYPE='en_US.UTF-8'
-export LC_ALL='en_US.UTF-8'
+export LANG='en_CA.UTF-8'
+export LC_CTYPE='en_CA.UTF-8'
+export LC_ALL='en_CA.UTF-8'
 
 # For standard sorting in ls, ...
 export LC_COLLATE=C
@@ -15,12 +15,9 @@ export LC_COLLATE=C
 # Don’t clear the screen after quitting a manual page
 #export MANPAGER="less -X"
 
-# Sorry Brew, but that's not something I want
-export HOMEBREW_NO_ANALYTICS=1
-
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    export GREP_OPTIONS="--color=auto"
-else
-    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-    #export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh    
+	# Sorry Brew, but that's not something I want
+	export HOMEBREW_NO_ANALYTICS=1
 fi
+
+export GREP_OPTIONS="--color=auto"
