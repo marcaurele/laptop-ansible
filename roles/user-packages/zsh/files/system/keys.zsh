acc9fef13004f286c39e9b926ede4207100a4194
Marc-Aurèle Brothier <m@brothier.org>
2019-01-23 09:21:40 +0100

    alias: add alias to copy gpg public key

--- roles/user-packages/zsh/files/system/keys.zsh
+++ roles/user-packages/zsh/files/system/keys.zsh
@@ -3,4 +3,5 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
 	alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 else
 	alias pubkey="cat ~/.ssh/id_rsa.pub | xclip -sel clip"
+	alias pubgkey="gpg --armor --export m@brothier.org | xclip -sel clip"
 fi
