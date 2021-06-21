b8e2847a159bf6f7bead6048887277c41a269c27
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2021-06-21 06:35:07 +0200

    package(pyenv): move install to pipx
    Seems a better option for the install.

--- roles/user/zsh/files/python/pyenv.zsh
+++ roles/user/zsh/files/python/pyenv.zsh
@@ -1,6 +1,7 @@
 # pyenv configuration
+export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$HOME/.pyenv/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
-	# eval "$(pyenv virtualenv-init -)"
 fi
