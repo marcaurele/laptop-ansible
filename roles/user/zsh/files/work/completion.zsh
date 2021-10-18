e0ac617a04898bf9345ddb4eaa8abf92f3f33a5e
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2021-10-18 12:18:04 +0200

    chore: fix aws-vault completion

--- roles/user/zsh/files/work/completion.zsh
+++ roles/user/zsh/files/work/completion.zsh
@@ -1,4 +1,24 @@
-# For aws-vault if present
-if type "aws-vault" > /dev/null; then
-	eval "$(aws-vault --completion-script-zsh)"
+#compdef aws-vault
+
+_aws-vault() {
+    local i
+    for (( i=2; i < CURRENT; i++ )); do
+        if [[ ${words[i]} == -- ]]; then
+            shift $i words
+            (( CURRENT -= i ))
+            _normal
+            return
+        fi
+    done
+
+    local matches=($(${words[1]} --completion-bash ${(@)words[2,$CURRENT]}))
+    compadd -a matches
+
+    if [[ $compstate[nmatches] -eq 0 && $words[$CURRENT] != -* ]]; then
+        _files
+    fi
+}
+
+if [[ "$(basename -- ${(%):-%x})" != "_aws-vault" ]]; then
+    compdef _aws-vault aws-vault
 fi
