8cb0f3985a5f0e1c138e71e7c5301af815865b4a
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2021-10-28 08:11:05 +0200

    chore: update pyenv completion

--- roles/user/zsh/files/python/pyenv.zsh
+++ roles/user/zsh/files/python/pyenv.zsh
@@ -1,6 +1,25 @@
 # pyenv configuration
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$HOME/.pyenv/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-	eval "$(pyenv init -)"
-fi
+#if command -v pyenv 1>/dev/null 2>&1; then
+#	eval "$(pyenv init -)"
+#fi
+export PYENV_SHELL=zsh
+source '/home/mbrothie/.pyenv/libexec/../completions/pyenv.zsh'
+command pyenv rehash 2>/dev/null
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")"
+    ;;
+  *)
+    command pyenv "$command" "$@"
+    ;;
+  esac
+}
