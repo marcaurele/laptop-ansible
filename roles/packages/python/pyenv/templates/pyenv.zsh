8c2c333e9cfd35af6e0a618325d9f6cd6b23f26f
Marc-Aurèle Brothier <m@brothier.org>
2022-03-11 21:45:39 +0100

    fix(pipenv): virtualenv in dir

--- roles/packages/python/pyenv/templates/pyenv.zsh
+++ roles/packages/python/pyenv/templates/pyenv.zsh
@@ -3,6 +3,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="${PATH}:$HOME/.pyenv/bin:$HOME/.pyenv/shims"
 export PYENV_SHELL=zsh
+export PIPENV_VENV_IN_PROJECT=1
 source "$HOME/.pyenv/completions/pyenv.zsh"
 command pyenv rehash 2>/dev/null
 pyenv() {
