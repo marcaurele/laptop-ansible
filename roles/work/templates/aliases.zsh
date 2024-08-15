075d016acee7dd601a5508a874e2adbf884be5b8
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2024-08-15 09:07:57 +0200

    new temp aliases

--- roles/work/templates/aliases.zsh
+++ roles/work/templates/aliases.zsh
@@ -1,2 +1,24 @@
 # {{ ansible_managed }}
 alias curljwt='curl --silent -H "Authorization: Bearer $JWT_TOKEN"'
+
+function aws_describe_instances_table () {
+  local env=$1
+  local type=$2
+  aws ec2 describe-instances \
+      --filters Name=tag:role,Values=$type Name=tag:Stack,Values=$env \
+      --query "Reservations[].Instances[].[PrivateIpAddress,InstanceId,LaunchTime,Placement.AvailabilityZone,[Tags[?Key=='Name'].Value][0][0],[Tags[?Key=='role'].Value][0][0]]" \
+      --output table
+}
+
+_aws_describe_instances_table_celery () {
+  local env=$1
+  aws ec2 describe-instances \
+      --filters Name=tag:role,Values=celery Name=tag:Stack,Values=$env \
+      --query "Reservations[].Instances[].[PrivateIpAddress,InstanceId,LaunchTime,Placement.AvailabilityZone,[Tags[?Key=='Name'].Value][0][0],[Tags[?Key=='role'].Value][0][0],[Tags[?Key=='subrole'].Value][0][0]]" \
+      --output table
+}
+
+alias aws-prod-django='aws_describe_instances_table prod django'
+alias aws-staging-django='aws_describe_instances_table staging django'
+alias aws-prod-celery='_aws_describe_instances_table_celery prod'
+alias aws-staging-celery='_aws_describe_instances_table_celery staging'
