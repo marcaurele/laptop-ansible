d41e67e27987ee64d4ced56145ecec5c0093d0f0
Marc-Aurèle Brothier <m@brothier.org>
2024-12-09 20:28:34 +0100

    feat: add alias for TF global plugin dir

--- roles/packages/hashicorp/tfenv/templates/tfenv.zsh
+++ roles/packages/hashicorp/tfenv/templates/tfenv.zsh
@@ -4,3 +4,4 @@ export PATH="$PATH:$HOME/.tfenv/bin"
 
 # Global Terraform provider directory to save space
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugins"
+alias tfproviders="terraform providers mirror $TF_PLUGIN_CACHE_DIR"
