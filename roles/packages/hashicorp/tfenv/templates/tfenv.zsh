e6cf2b052b2e9e8b4699ec506f63c1bc7a6af1df
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2025-07-17 15:24:37 +0200

    chore(package): remove tfenv

--- roles/packages/hashicorp/tfenv/templates/tfenv.zsh
+++
@@ -1,7 +0,0 @@
-# tfenv configuration
-# {{ ansible_managed }}
-export PATH="$PATH:$HOME/.tfenv/bin"
-
-# Global Terraform provider directory to save space
-export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugins"
-alias tfproviders="terraform providers mirror $TF_PLUGIN_CACHE_DIR"
