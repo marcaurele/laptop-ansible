e242e6ccbab35ac59c039b67f95d361f5e6bfb97
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2025-07-27 10:17:08 +0200

    feat: abbr for apt

--- roles/user/config/templates/config.fish
+++ roles/user/config/templates/config.fish
@@ -1,3 +1,5 @@
 # {{ ansible_managed }}
 
 abbr --add gitlab-registry-auth -- "pass web/com/gitlab/marcaurele-token-registry-ro | docker login registry.gitlab.com -u marcaurele --password-stdin"
+
+abbr --add apt-update -- "sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get dist-upgrade --yes; and sudo apt-get autoremove --purge --yes; and sudo apt-get clean"
