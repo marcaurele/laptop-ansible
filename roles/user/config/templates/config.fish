f08625c93a2d2ed211ba43d38d26f862fb577a7c
Marc-Aurèle Brothier <m@brothier.org>
2025-09-21 21:16:15 +0200

    feat(pkg): add snapstore

--- roles/user/config/templates/config.fish
+++ roles/user/config/templates/config.fish
@@ -3,3 +3,5 @@
 abbr --add gitlab-registry-auth -- "pass web/com/gitlab/marcaurele-token-registry-ro | docker login registry.gitlab.com -u marcaurele --password-stdin"
 
 abbr --add apt-update -- "sudo apt-get update; and sudo apt-get upgrade --yes; and sudo apt-get dist-upgrade --yes; and sudo apt-get autoremove --purge --yes; and sudo apt-get clean"
+
+set -x PATH $PATH /snap/bin
