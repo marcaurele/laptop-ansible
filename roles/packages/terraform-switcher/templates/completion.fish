2109958cdd8c515f70571bb1e87501f6a64e9a10
Marc-Aurèle Brothier <marc-aurele.brothier@pix4d.com>
2025-12-02 14:39:27 +0100

    feat(pkg): terraform-switcher / tfswitch

---
+++ roles/packages/terraform-switcher/templates/completion.fish
@@ -0,0 +1,25 @@
+# {{ ansible_managed }}
+# Translated from Bash completion in repository
+complete -c tfswitch -f
+
+# --- Flag: -A / --arch ---
+complete -c tfswitch -s A -l arch -d "Select architecture" -r -f -a "386 amd64 arm arm64"
+
+# --- Flag: -b / --bin ---
+complete -c tfswitch -s b -l bin -d "Custom binary path" -r
+
+# --- Flag: -c / --chdir AND -i / --install ---
+complete -c tfswitch -s c -l chdir -d "Switch to a different directory" -r -a "(__fish_complete_directories)"
+complete -c tfswitch -s i -l install -d "Install specific version" -r -a "(__fish_complete_directories)"
+
+# --- Flag: -g / --log-level ---
+complete -c tfswitch -s g -l log-level -d "Log level" -r -f -a "DEBUG ERROR FATAL INFO NOTICE OFF PANIC TRACE WARN"
+
+# --- Flag: -t / --product ---
+complete -c tfswitch -s t -l product -d "Product to switch" -r -f -a "opentofu terraform"
+
+# --- General Flags (Parsed from help in Bash) ---
+complete -c tfswitch -s l -l list -d "List all installed versions"
+complete -c tfswitch -s u -l list-all -d "List all available versions"
+complete -c tfswitch -s h -l help -d "Show help"
+complete -c tfswitch -s v -l version -d "Show version"
